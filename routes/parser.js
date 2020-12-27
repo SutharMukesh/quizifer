67ac652b85b0636350b630eaba870e77751964a6
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-12-27 15:53:05 +0530

    add support for custom cardBackgroud

--- routes/parser.js
+++ routes/parser.js
@@ -1,13 +1,15 @@
 const hljs = require("highlight.js");
 const Md = require("markdown-it");
 
-function getDesiredConfig(options={}) {
+function getDesiredConfig(options = {}) {
 	const opt = {};
 	opt.theme = options.theme == 1 ? "stackoverflow-light" : "stackoverflow-dark";
 	opt.fontFamily = options.fontFamily || "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif";
 	opt.fontSize = options.fontSize || "14px";
 	opt.fontWeight = options.fontWeight || "normal";
-	return opt
+	opt.cardBackground = options.cardBackground || "#ffffff00";
+	opt.cardTopColor = options.theme == 1 ? "#FFFFFF" : "#000000";
+	return opt;
 }
 
 module.exports = function (data, options) {
@@ -42,21 +44,20 @@ module.exports = function (data, options) {
 
     <style>
     body{
-		background: #F1F2F8;
 		font-family: ${config.fontFamily};
 		font-size: ${config.fontSize};
 		font-weight: ${config.fontWeight};
     }
     .container{
-        margin: 30px;
+		margin: 30px;
         padding: 30px;
-        background: #F9F9FA;
-        box-shadow: 0px 13px 31px rgba(12, 20, 33, 0.04), 0px 9.45547px 20.8947px rgba(12, 20, 33, 0.032375), 0px 6.58125px 13.5141px rgba(12, 20, 33, 0.027), 0px 4.31641px 8.38574px rgba(12, 20, 33, 0.023125), 0px 2.6px 5.0375px rgba(12, 20, 33, 0.02), 0px 1.37109px 2.99707px rgba(12, 20, 33, 0.016875), 0px 0.56875px 1.79219px rgba(12, 20, 33, 0.013), inset 0px 2px 1px #FFFFFF;
-        border-radius: 20px;
+        background: ${config.cardBackground};
+        box-shadow: 0px 13px 31px rgba(12, 20, 33, 0.04), 0px 9.45547px 20.8947px rgba(12, 20, 33, 0.032375), 0px 6.58125px 13.5141px rgba(12, 20, 33, 0.027), 0px 4.31641px 8.38574px rgba(12, 20, 33, 0.023125), 0px 2.6px 5.0375px rgba(12, 20, 33, 0.02), 0px 1.37109px 2.99707px rgba(12, 20, 33, 0.016875), 0px 0.56875px 1.79219px rgba(12, 20, 33, 0.013);
+        border-radius: 1.25rem;
     }
     pre {
         padding: 1rem !important;
-        border-radius: 3px;
+        border-radius: 5px;
     }
     code{
 		display: inline !important;
@@ -65,6 +66,6 @@ module.exports = function (data, options) {
 	}
     </style>
 	</html>`;
-	console.log(html);
 	return html;
+
 };
