3ab52a4ed34d27b26074ccffd8a17b5c289b9b6e
Mukesh Suthar <mukeshsuthar336@gmail.com>
2021-01-21 08:41:36 +0530

    API: remove html css code

--- api/index.js
+++ api/index.js
@@ -3,10 +3,8 @@ const express = require("express");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
-require("./database");
-const config = require("./config");
 const qotd = require("./routes/qotd");
+require("./database");
 
 const app = express();
 app.use(helmet());
@@ -14,9 +12,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use("/qotd", qotd);
-
-// app.listen(config.server.port, () => {
-// 	console.log(`Quizifer listens on port ${config.server.port}`);
-// });
 
 module.exports.handler = serverless(app);
--- api/routes/parser.js
+++ api/routes/parser.js
@@ -1,18 +1,7 @@
 const hljs = require("highlight.js");
 const Md = require("markdown-it");
 
-function getDesiredConfig(options = {}) {
-	const opt = {};
-	opt.theme = options.theme == 1 ? "stackoverflow-light" : "stackoverflow-dark";
-	opt.fontFamily = options.fontFamily || "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif";
-	opt.fontSize = options.fontSize || "14px";
-	opt.fontWeight = options.fontWeight || "normal";
-	opt.cardBackground = options.cardBackground || "#ffffff00";
-	opt.cardTopColor = options.theme == 1 ? "#FFFFFF" : "#000000";
-	return opt;
-}
-
-module.exports = function (data, options) {
+module.exports = async function (data, options) {
 	let md = Md({
 		html: true,
 		highlight: function (str, lang) {
@@ -27,45 +16,6 @@ module.exports = function (data, options) {
 	});
 
 	const renderedQuestion = md.render(data);
-	const config = getDesiredConfig(options);
-
-	const html = `<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/styles/${config.theme}.min.css">
-	</head>
-    <body>
-        <div class="container">
-            ${renderedQuestion.replace(/<code>/g, '<code class="hljs">')}
-        </div>
-    </body>
-
-    <style>
-    body{
-		font-family: ${config.fontFamily};
-		font-size: ${config.fontSize};
-		font-weight: ${config.fontWeight};
-    }
-    .container{
-		margin: 30px;
-        padding: 30px;
-        background: ${config.cardBackground};
-        box-shadow: 0px 13px 31px rgba(12, 20, 33, 0.04), 0px 9.45547px 20.8947px rgba(12, 20, 33, 0.032375), 0px 6.58125px 13.5141px rgba(12, 20, 33, 0.027), 0px 4.31641px 8.38574px rgba(12, 20, 33, 0.023125), 0px 2.6px 5.0375px rgba(12, 20, 33, 0.02), 0px 1.37109px 2.99707px rgba(12, 20, 33, 0.016875), 0px 0.56875px 1.79219px rgba(12, 20, 33, 0.013);
-        border-radius: 1.25rem;
-    }
-    pre {
-        padding: 1rem !important;
-        border-radius: 5px;
-    }
-    code{
-		display: inline !important;
-		padding: 0 0.5rem !important;
-        border-radius: 2px;
-	}
-    </style>
-	</html>`;
+	const html = renderedQuestion.replace(/<code>/g, '<code class="hljs">');
 	return html;
-
 };
