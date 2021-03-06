fd1dd33b64be3b3dde7fb34130b564fd84c6be64
Mukesh Suthar <mukeshsuthar336@gmail.com>
2021-03-06 14:05:27 +0530

    Ext: After sign in page changed.

--- extension/src/authenticate.ts
+++ extension/src/authenticate.ts
@@ -17,7 +17,34 @@ export function authenticate(fn: () => void) {
 
 		await StateManager.setState("accessToken", token);
 		fn();
-		res.end(`<h3>Auth was Successful, you can close this window</h3>`);
+		res.end(`<!doctype html>
+		<html lang="en">
+		  <head>
+			<meta charset="utf-8">
+			<meta
+			  http-equiv="Content-Security-Policy"
+			  content="default-src vscode-resource:; form-action vscode-resource:; frame-ancestors vscode-resource:; img-src vscode-resource: https:; script-src 'self' 'unsafe-inline' vscode-resource:; style-src 'self' 'unsafe-inline' vscode-resource:;"
+			/>
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		  </head>
+		  <body>
+			  <h1>Login success!</h1>
+			  <h2>You can now close this tab.</h2>
+			  <style>
+				html, body {
+				  background-color: #1a1a1a;
+				  color: #c3c3c3;
+				  display: flex;
+				  flex-direction: column;
+				  justify-content: center;
+				  align-items: center;
+				  height: 100%;
+				  width: 100%;
+				  margin: 0;
+				}
+			  </style>
+		  </body>
+		</html>`);
 
 		app.server?.close();
 	});
